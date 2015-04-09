e1007e9850740553e3fa193a5ea493103fedace5
Tyler Zupan <tzupan@ql1maprpr3.mi.corp.rockfin.com>
2015-04-09 12:33:29 -0400

    added more notes

--- practiceTkiner.py
+++ practiceTkiner.py
@@ -24,6 +24,25 @@ label1.pack()
 # a frame as in invisible container that can be places in a window, this creates frame, still needs to be placed
 topFrame = Frame(root)
 
+#########################################################
+#use entry to allow for user input/blank fields
+entryExample = Entry(root,)
+
+########################################################
+# can use grids to organize widgets
+#column default is 0
+#can align within container using 'sticky' = N, E, W, S (north, east, south, west)
+widg1.grid(row=0)
+widg2.grid(row=0, column=1)
+widg3.grid(row=1)
+widg4.grid(row=1, column=1)
+
+#########################################################
+#to get a checkbox
+exampleCheck = Checkbutton(root, text='example text')
+#how to make a widget span multiple cells
+exampleCheck.grid(columnspan=2)
+
 ##########################################################
 #buttons
 #button1 = Button(where to put it, text of button, color of button)
